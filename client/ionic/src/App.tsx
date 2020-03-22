f0eadd807c7ee3bf01a837dd54a7ba543d1608b6
Yosua Ian Sebastian <yosua.ian.sebastian@gmail.com>
2020-03-22 12:52:31 +0700

    Make UI respect top safe space

--- client/ionic/src/App.tsx
+++ client/ionic/src/App.tsx
@@ -27,6 +27,9 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+/* Global styling */
+import './global.css';
+
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
